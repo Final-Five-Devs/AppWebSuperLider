0ed5c6d07edcd8377ab41ba1fbc3af3edecb7052
FAMILIA <kvivi1407@gmail.com>
2022-10-14 12:18:43 -0500

    Primer commit by Viviana De Los Rios

--- js/commitVivianaDeLosRios.js
+++ js/commitVivianaDeLosRios.js
@@ -1,6 +1,6 @@
-let edad = 28;
-let ciudad = "Medellín";
-let nombre = "Kely viviana De Los Rios";
+let edad_ = 28;
+let ciudad_ = "Medellín";
+let nombre_ = "Kely viviana De Los Rios";
 
 
-console.log(`Hola mi nombre es ${nombre} tengo ${edad} años y vivo en ${ciudad}`);
+console.log(`Hola mi nombre es ${nombre_} tengo ${edad_} años y vivo en ${ciudad_}`);
