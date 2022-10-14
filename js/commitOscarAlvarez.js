0255571039defab825369e3e8ee79bc475cec27e
OscarAlvarezV <oscar.alvarez.vi@gmaqil.com>
2022-10-13 20:38:37 -0500

    Primer commit by Oscar Alvarez

---
+++ js/commitOscarAlvarez.js
@@ -0,0 +1,6 @@
+let annos = 34;
+let ciudaDeResidencia = "Bogotá D.C";
+let nombres = "Oscar Alvarez"
+
+console.log("Hola mi nombre es: " + nombres + " tengo " + annos + " años y vivo en " + ciudaDeResidencia);
+
