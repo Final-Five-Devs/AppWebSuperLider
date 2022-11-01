01a4a0fcb503cac81b826400c7e6f9c76bac2d5e
Viviana De Los Rios <111185518+kdelosrios@users.noreply.github.com>
2022-11-01 18:52:30 -0500

    Update index.js
    se cambia comando para inicializar proyecto

--- Front/src/index.js
+++ Front/src/index.js
@@ -12,9 +12,9 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
